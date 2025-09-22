618cf6b5601abf6715fca0227443b23c937902d3
Paul Jonathan <pauljonathan061204@gmail.com>
2025-09-22 23:19:19 +0700

    Changed the scraping to stop automatically when its on the last page

--- web_scraping.py
+++ web_scraping.py
@@ -12,11 +12,12 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070213 BonEcho/2.0.0.2pre'
 }
 
+page_url = 'https://quotes.toscrape.com/page/1'
 author_links = []
 
-for x in range(1, 11):
+while page_url:
 
-    r = requests.get(f'https://quotes.toscrape.com/page/{x}') # Current page we want to scrape
+    r = requests.get(page_url) # Current page we want to scrape
     soup = BeautifulSoup(r.content, 'lxml')
 
     quotes = soup.find_all('div', class_='quote')
@@ -26,6 +27,14 @@ for x in range(1, 11):
 
     for author in author_set:
         author_links.append(baseURL + author['href'])
+    
+    next_page = soup.find('li', class_='next')
+    
+    if next_page:
+        page_url = (baseURL + next_page.find('a', href=True)['href'])
+    else:
+        print('Last page reached')
+        page_url = None
 
 author_links = list(set(author_links))
 printer.pprint(author_links)
