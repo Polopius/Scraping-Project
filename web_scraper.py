904b21465a61276297793f0599e96227abc673c9
Paul Jonathan <pauljonathan061204@gmail.com>
2025-09-23 08:50:50 +0700

    Changed scraped data to a quote text, author name, and tags

--- web_scraper.py
+++ web_scraper.py
@@ -13,28 +13,34 @@ headers = {
 }
 
 page_url = 'https://quotes.toscrape.com/page/1'
-author_links = []
+quotes_data_list = []
 
 while page_url:
-
     r = requests.get(page_url) # Current page we want to scrape
     soup = BeautifulSoup(r.content, 'lxml')
 
     quotes = soup.find_all('div', class_='quote')
-    # print(quotes)
-
-    author_set = {link for item in quotes for span in item.find_all('span') for link in span.find_all('a', href=True)}
-
-    for author in author_set:
-        author_links.append(baseURL + author['href'])
+    
+    for item in quotes:
+        # Get quotes, author name, and tags
+        quote_text = item.find('span', class_='text').text.strip()
+        
+        author_name = item.find('small', class_='author').text.strip()
+        
+        tags = [tag.text.strip() for container in item.find_all('div', class_='tags') for tag in container.find_all('a', class_='tag')]
+        
+        # Append to quote data list
+        quotes_data_list.append({
+            'quotes' : quote_text,
+            'author' : author_name,
+            'tags' : tags
+        })
     
     next_page = soup.find('li', class_='next')
-    
     if next_page:
         page_url = (baseURL + next_page.find('a', href=True)['href'])
     else:
         print('Last page reached')
         page_url = None
 
-author_links = list(set(author_links))
-printer.pprint(author_links)
+printer.pprint(quotes_data_list)
